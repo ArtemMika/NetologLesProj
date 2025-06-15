90a5d4f0ff7437bae21ae024ab2d16931888400e
ArtemMika <90694660+ArtemMika@users.noreply.github.com>
2025-06-16 01:30:02 +0300

    Add files via upload

---
+++ ProjLe3/ProjLe3/ProjLe3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    std::cout << "Оператор: ||" << std::endl;
+
+    std::cout << "true\t" << "true\t" << ((true || true) ? "true" : "false") << std::endl;
+    std::cout << "true\t" << "false\t" << ((true || false) ? "true" : "false") << std::endl;
+    std::cout << "false\t" << "true\t" << ((false || true) ? "true" : "false") << std::endl;
+    std::cout << "false\t" << "false\t" << ((false || false) ? "true" : "false") << std::endl;
+
+    std::cout << "\nОператор: &&" << std::endl;
+
+    std::cout << "true\t" << "true\t" << ((true && true) ? "true" : "false") << std::endl;
+    std::cout << "true\t" << "false\t" << ((true && false) ? "true" : "false") << std::endl;
+    std::cout << "false\t" << "true\t" << ((false && true) ? "true" : "false") << std::endl;
+    std::cout << "false\t" << "false\t" << ((false && false) ? "true" : "false") << std::endl;
+
+    return 0;
+}
---
+++ ProjLes3-2/ProjLes3-2/ProjLes3-2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+
+    int a, b, c;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе число: ";
+    std::cin >> b;
+    std::cout << "Введите третье число: ";
+    std::cin >> c;
+
+    std::cout << "Результат: ";
+
+    int max = (a >= b && a >= c) ? a : ((b >= a && b >= c) ? b : c);
+
+    int min = (a <= b && a <= c) ? a : ((b <= a && b <= c) ? b : c);
+
+    int mid = a + b + c - max - min;
+
+    std::cout << (max >= mid ? max : mid) << " "; 
+    std::cout << (min <= mid ? mid : min) << " ";
+    std::cout << (min <= mid ? min : mid) << std::endl;
+
+    return 0;
+}
