683142e1ec10930924a9dbd8eee28e6e4885e062
ArtemMika <90694660+ArtemMika@users.noreply.github.com>
2025-06-23 00:58:19 +0300

    Add files via upload

---
+++ Les4/Les4/Les4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number{};
+	int sum{0};
+	do
+	{
+		std::cout << "Введите целое число или число '0', чтобы закончить: " << "\n";
+		std::cin >> number;
+
+		if (number != 0)
+		{
+			sum += number;
+		}
+	} while (number != 0);
+
+	std::cout << "Сумма: " << sum;
+
+	return 0;
+}
